603c363e33d013e6e41b796bcf1a72e29f1d6310
Bibhukesh Medhi <2102508728@svyasa-sas.edu.in>
2026-01-14 14:11:22 +0530

    added happy Tummy

---
+++ fat_tummy.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn")
+let msg = document.createElement("h2")
+
+btn.addEventListener("click",B)
+
+function B(){
+    // msg.innerText = "You Sucessfully Loged in!";
+    // msg.style.color = "green";
+    alert("Successfully Logged in!")
+}
+
+
